d3b879b7df2303c4009e33501cebe5b986213ceb
psqq <pestov-sa@ya.ru>
2017-01-26 23:04:55 +0300

    start coding DigraphIso class

--- code/graph_iso.cpp
+++ code/graph_iso.cpp
@@ -2,13 +2,25 @@
 #include "digraph.h"
 using namespace std;
 
-bool is_iso(digraph g1, digraph g2) {
-  if (g1.count_vertices() != g2.count_vertices()) {
-    return false;
+class DigraphIso {
+  digraph g1, g2;
+  bool match(int k) {}
+  bool go(set<int> s) {}
+public:
+  DigraphIso(digraph ag1, digraph ag2) : g1(ag1), g2(ag2) {}
+  bool is_iso() {
+    if (g1.count_vertices() != g2.count_vertices()) {
+      return false;
+    }
   }
-}
+};
 
 int main() {
-  digraph g("1.digraph");
-  g.print();
+  digraph g1("1.digraph"), g2("2.digraph");
+  cout << "# 1.digraph:\n";
+  g1.print();
+  cout << "# 2.digraph:\n";
+  g2.print();
+  DigraphIso iso(g1, g2);
+  cout << "Is isomorph?: " << iso.is_iso() << endl;
 }
