df3af3a105faa847281c093144f53adf55095347
psqq <pestov-sa@ya.ru>
2017-03-11 17:00:29 +0300

    тест для add_edge и is_edge

--- code/tests/MyTestSuite.h
+++ code/tests/MyTestSuite.h
@@ -1,17 +1,25 @@
 #include "cxxtest/TestSuite.h"
 #include "digraph.h"
+#include <cstdlib>
+#include <set>
+using namespace std;
 
 class MyTestSuite : public CxxTest::TestSuite {
 public:
-  void test_SimpleDigraph() {
+  void test_SimpleDigraph_Constructor() {
     SimpleDigraph g1;
     TS_ASSERT_EQUALS(g1.number_of_nodes(), 0);
     TS_ASSERT_EQUALS(g1.number_of_edges(), 0);
-    SimpleDigraph g2(2);
-    TS_ASSERT_EQUALS(g2.number_of_nodes(), 2);
+    SimpleDigraph g2(1);
+    TS_ASSERT_EQUALS(g2.number_of_nodes(), 1);
     TS_ASSERT_EQUALS(g2.number_of_edges(), 0);
+    int n = rand() % 100;
+    SimpleDigraph g3(n);
+    TS_ASSERT_EQUALS(g3.number_of_nodes(), n);
+    TS_ASSERT_EQUALS(g3.number_of_edges(), 0);
   }
-  void test_SimpleDigraph_add_node() {
+
+  void test_SimpleDigraph_METHODS_add_node() {
     SimpleDigraph g1;
     TS_ASSERT_EQUALS(g1.number_of_nodes(), 0);
     TS_ASSERT_EQUALS(g1.number_of_edges(), 0);
@@ -23,5 +31,32 @@ public:
     TS_ASSERT_EQUALS(v, 1);
     TS_ASSERT_EQUALS(g1.number_of_nodes(), 2);
     TS_ASSERT_EQUALS(g1.number_of_edges(), 0);
+  }
+
+  void test_SimpleDigraph_METHODS_add_edge_AND_is_edge() {
+    SimpleDigraph g1(5);
+    g1.add_edge(0, 1);
+    g1.add_edge(0, 2);
+    g1.add_edge(3, 4);
+    TS_ASSERT_EQUALS(g1.number_of_nodes(), 5);
+    TS_ASSERT_EQUALS(g1.number_of_edges(), 3);
+    int n = 5 + rand() % 10, m = rand() % n;
+    set<pair<int, int>> edges;
+    for (int i = 0; i < m; i++) {
+      edges.emplace(rand() % n, rand() % n);
+    }
+    m = edges.size();
+    SimpleDigraph g2(n);
+    for (auto &p : edges) {
+      g2.add_edge(p.first, p.second);
+    }
+    TS_ASSERT_EQUALS(g2.number_of_nodes(), n);
+    TS_ASSERT_EQUALS(g2.number_of_edges(), m);
+    for (int u = 0; u < n; u++) {
+      for (int v = 0; v < n; v++) {
+        TS_ASSERT_EQUALS(g2.is_edge(u, v),
+                         edges.find(make_pair(u, v)) != edges.end());
+      }
+    }
   }
 };
--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -25,19 +25,25 @@ bool suite_MyTestSuite_init = false;
 static MyTestSuite suite_MyTestSuite;
 
 static CxxTest::List Tests_MyTestSuite = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "MyTestSuite.h", 4, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
+CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "MyTestSuite.h", 7, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
 
-static class TestDescription_suite_MyTestSuite_test_SimpleDigraph : public CxxTest::RealTestDescription {
+static class TestDescription_suite_MyTestSuite_test_SimpleDigraph_Constructor : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_test_SimpleDigraph() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 6, "test_SimpleDigraph" ) {}
- void runTest() { suite_MyTestSuite.test_SimpleDigraph(); }
-} testDescription_suite_MyTestSuite_test_SimpleDigraph;
+ TestDescription_suite_MyTestSuite_test_SimpleDigraph_Constructor() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 9, "test_SimpleDigraph_Constructor" ) {}
+ void runTest() { suite_MyTestSuite.test_SimpleDigraph_Constructor(); }
+} testDescription_suite_MyTestSuite_test_SimpleDigraph_Constructor;
 
-static class TestDescription_suite_MyTestSuite_test_SimpleDigraph_add_node : public CxxTest::RealTestDescription {
+static class TestDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_node : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_MyTestSuite_test_SimpleDigraph_add_node() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 14, "test_SimpleDigraph_add_node" ) {}
- void runTest() { suite_MyTestSuite.test_SimpleDigraph_add_node(); }
-} testDescription_suite_MyTestSuite_test_SimpleDigraph_add_node;
+ TestDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_node() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 21, "test_SimpleDigraph_METHODS_add_node" ) {}
+ void runTest() { suite_MyTestSuite.test_SimpleDigraph_METHODS_add_node(); }
+} testDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_node;
+
+static class TestDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_edge_AND_is_edge : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_edge_AND_is_edge() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 34, "test_SimpleDigraph_METHODS_add_edge_AND_is_edge" ) {}
+ void runTest() { suite_MyTestSuite.test_SimpleDigraph_METHODS_add_edge_AND_is_edge(); }
+} testDescription_suite_MyTestSuite_test_SimpleDigraph_METHODS_add_edge_AND_is_edge;
 
 #include <cxxtest/Root.cpp>
 const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
