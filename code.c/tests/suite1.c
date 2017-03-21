fbe6568b780a1933b966ef20b920096d23f5d9e7
psqq <pestov-sa@ya.ru>
2017-03-21 18:28:09 +0300

    написал генератор тестов

--- code.c/tests/suite1.c
+++ code.c/tests/suite1.c
@@ -6,7 +6,13 @@ int clean_suite1(void) { return 0; }
 
 // test: create and free digraph
 void test_digraph_create(void) {
-  digraph *g = digraph_create(0);
-  CU_ASSERT(g != NULL);
-  digraph_free(g);
+  // digraph *g = digraph_create(0);
+  // CU_ASSERT(g != NULL);
+  // digraph_free(g);
+}
+
+// test: 2 plus 2 == 4
+void test_2_plus_2(void) {
+  int x = 2 + 2;
+  CU_ASSERT(x == 4);
 }
