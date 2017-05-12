ed925947d845a0cdd1c04e99f8c7e62da091fce9
psqq <pestov-sa@ya.ru>
2017-05-12 20:05:51 +0300

    упростил парсинг аргументов командной строки; перзентация; новая статья; новые ссылки в readme

--- code/src/semi.cpp
+++ code/src/semi.cpp
@@ -2,9 +2,12 @@
 #include "semiiso.h"
 #include "semilog.h"
 #include "utils.h"
+#include "inv3.h"
 #include <cstring>
 #include <fstream>
 #include <set>
+#include <map>
+#include <functional>
 using namespace std;
 
 const char *usage_ru = R"EOF(Usage help:
@@ -13,13 +16,17 @@ semi tos f1 - преобразует орграф из файла f1 в полу
 semi tog f1 - преобразует полурешетку из файла f1 в орграф.
 semi giso f1 f2 - проверить орграфы из файлов f1 и f2 на изоморфизм.
 semi todot f1 - преобразует орграф в формат .dot файл для graphviz.
+semi inv3 s.txt - выводит инвариант 3 для полурешетки из файла s.txt
+semi ginv3 g.txt - выводит инвариант 3 для графа полурешетки из файла g.txt
+
+-speed - выводит время выполнения программы (в секундах): "speed: 0.023"
 
 -log - добавьте этот флаг для ведения программой подробного лога. Все
 данные будут записываться в файл log.txt. Если такой файл существует, то
 он будет перезаписан.
 
--int - используйте этот флаг, когда все элементы - целые числа, иначе элементами
-будут строки.
+-int - используйте этот флаг, когда все элементы - целые числа (включен по
+умолчанию), иначе элементами будут строки.
 
 -str - элементы графов и полурешеток - строки.
 
@@ -47,6 +54,15 @@ void parse_args(int argc, char **argv) {
   }
   count_cmd_args = cmd_args.size();
 }
+
+#define USE_FILE_FOR_OUTPUT_IF_EXISTS(cmd_args_index, osvar) \
+  ostream *osvar ## _ost = &cout; \
+  ofstream osvar ## _fout; \
+  if (cmd_args_index < count_cmd_args) { \
+    osvar ## _fout.open(cmd_args[cmd_args_index]); \
+    osvar ## _ost = & osvar ## _fout; \
+  } \
+  ostream &osvar = *osvar ## _ost
 
 template <class T> int iso() {
   if (count_cmd_args < 2) {
@@ -82,13 +98,7 @@ template <class T> int tog() {
   Semilattice<T> s = Semilattice<T>::from_file(cmd_args[0]);
   Digraph<T> g = to_digraph(s);
 
-  ostream *ost = &cout;
-  ofstream fout;
-  if (count_cmd_args == 2) {
-    fout.open(cmd_args[1]);
-    ost = &fout;
-  }
-  ostream &os = *ost;
+  USE_FILE_FOR_OUTPUT_IF_EXISTS(1, os);
 
   os << g.to_string();
   return 0;
@@ -100,16 +110,13 @@ template <class T> int todot() {
     return 1;
   }
   Digraph<T> g = Digraph<T>::from_file(cmd_args[0]);
-  ostream *ost = &cout;
-  ofstream fout;
-  if (count_cmd_args == 2) {
-    fout.open(cmd_args[1]);
-    ost = &fout;
-  }
-  ostream &os = *ost;
+
+  USE_FILE_FOR_OUTPUT_IF_EXISTS(1, os);
+
   os << R"EOF(digraph {
-node [ shape=circle ]
-rankdir = BT
+node [ shape=circle ];
+// edge [ arrowhead="none" ];
+rankdir = BT;
 )EOF";
   set<T> used;
   for (auto p : g.edges()) {
@@ -118,24 +125,48 @@ rankdir = BT
     used.insert(p.second);
   }
   for (auto v : g.nodes()) {
-    if (used.find(v) != used.end()) continue;
+    if (used.find(v) != used.end())
+      continue;
     os << v << endl;
   }
   os << "}" << endl;
   return 0;
 }
 
+template <class T> int inv3() {
+  if (count_cmd_args < 1) {
+    cout << "too few arguments" << endl;
+    return 1;
+  }
+  Semilattice<T> s = Semilattice<T>::from_file(cmd_args[0]);
+  Digraph<T> g = to_digraph(s);
+
+  USE_FILE_FOR_OUTPUT_IF_EXISTS(1, os);
+
+  Inv3<T> inv3_for_g(g);
+
+  os << inv3_for_g.get_full_inv3();
+}
+
+template <class T> int ginv3() {
+  if (count_cmd_args < 1) {
+    cout << "too few arguments" << endl;
+    return 1;
+  }
+  Digraph<T> g = Digraph<T>::from_file(cmd_args[0]);
+
+  USE_FILE_FOR_OUTPUT_IF_EXISTS(1, os);
+
+  Inv3<T> inv3_for_g(g);
+
+  os << inv3_for_g.get_full_inv3();
+}
+
 int main(int argc, char **argv) {
   parse_args(argc, argv);
-  set<string> all_cmds = {"iso", "tos", "tog", "giso", "todot"};
 
   // cout << "cmd: " << cmd << endl;
   // cout << "cmd_args: " << cmd_args << endl;
-
-  if (argc == 1 || all_cmds.find(cmd) == all_cmds.end()) {
-    cout << usage_ru;
-    return 0;
-  }
 
   // cout << "log_mode: " << log_mode << endl;
 
@@ -164,40 +195,27 @@ int main(int argc, char **argv) {
 
   try {
 
-    if (!fast_mode) {
-      if (cmd == "iso") {
-        if (int_mode) {
-          return iso<int>();
-        } else {
-          return iso<string>();
-        }
-        return 0;
-      } else if (cmd == "tos") {
-        if (int_mode) {
-          return tos<int>();
-        } else {
-          return tos<string>();
-        }
-        return 0;
-      } else if (cmd == "tog") {
-        if (int_mode) {
-          return tog<int>();
-        } else {
-          return tog<string>();
-        }
-        return 0;
-      } else if (cmd == "todot") {
-        if (int_mode) {
-          return todot<int>();
-        } else {
-          return todot<string>();
-        }
-        return 0;
-      }
-    } else { // fast mode
-      cout << "fast mode don't work" << endl;
-      return 0;
+#define CMD(f) { #f, { f<int>, f<string> } }
+
+    map<string, vector<function<int()>>> cmds = {
+      CMD(iso), CMD(tos), CMD(tog), CMD(todot), CMD(inv3), CMD(ginv3),
+    };
+
+#undef CMD
+
+    if (cmds.find(cmd) == cmds.end()) {
+      cout << "Command \"" << cmd << "\" not found. Usage:" << endl;
+      cout << usage_ru;
+      return 1;
     }
+
+    if (fast_mode) {
+      cout << "fast mode doesn't work." << endl;
+    }
+
+    int j = 0;
+    if (int_mode == false) j = 1;
+    return cmds[cmd][j]();
 
   } catch (exception &e) {
     cout << "Exception: " << e.what() << endl;
